a10ca4484c90f9687f16a074ea3409ee0ab8793a
Isaac <Thongchai.se@kkumail.com>
2025-09-01 01:04:51 +0700

    Lab8

---
+++ Lab8_663380595-6/src/main/java/com/customer/Lab76633805956Application.java
@@ -0,0 +1,13 @@
+package com.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab76633805956Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab76633805956Application.class, args);
+	}
+
+}
